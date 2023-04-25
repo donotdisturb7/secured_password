ab1a5196b7282c5065d1ee4abcf9f261c4852444
LetMeBeZen <84046981+donotdisturb7@users.noreply.github.com>
2023-04-25 10:34:01 -0400

    final

--- main.py
+++ main.py
@@ -43,15 +43,14 @@ class App(customtkinter.CTk):
 
         # logo de l'application
         self.logo_image = customtkinter.CTkImage(Image.open(
-            os.path.join(image_path, "secure.png")), size=(26, 26))
-
+            os.path.join(image_path, "Lg.jpg")), size=(26, 26))
+        
         # image pour la page d'accueil
         self.large_test_image = customtkinter.CTkImage(Image.open(
-            os.path.join(image_path, "large_test_image.png")), size=(500, 150))
+            os.path.join(image_path, "Lg.jpg")), size=(750, 750))
 
-        # image pour le menu de changement de thème
-        self.image_icon_image = customtkinter.CTkImage(Image.open(
-            os.path.join(image_path, "image_icon_light.png")), size=(20, 20))
+
+       
 
         # images pour les boutons de navigation
         self.home_image = customtkinter.CTkImage(light_image=Image.open(
@@ -122,23 +121,12 @@ class App(customtkinter.CTk):
         self.home_frame.grid_columnconfigure(0, weight=1)
 
         self.home_frame_large_image_label = customtkinter.CTkLabel(
-            self.home_frame, text="", image=self.large_test_image)
+        self.home_frame, text="", image=self.large_test_image)
         self.home_frame_large_image_label.grid(
             row=0, column=0, padx=20, pady=10)
 
-        self.home_frame_button_1 = customtkinter.CTkButton(
-            self.home_frame, text="", image=self.image_icon_image)
-        self.home_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
-        self.home_frame_button_2 = customtkinter.CTkButton(
-            self.home_frame, text="CTkButton", image=self.image_icon_image, compound="right")
-        self.home_frame_button_2.grid(row=2, column=0, padx=20, pady=10)
-        self.home_frame_button_3 = customtkinter.CTkButton(
-            self.home_frame, text="CTkButton", image=self.image_icon_image, compound="top")
-        self.home_frame_button_3.grid(row=3, column=0, padx=20, pady=10)
-        self.home_frame_button_4 = customtkinter.CTkButton(
-            self.home_frame, text="CTkButton", image=self.image_icon_image, compound="bottom", anchor="w")
-        self.home_frame_button_4.grid(row=4, column=0, padx=20, pady=10)
-
+        
+        
         # création de la frame2
         self.second_frame = customtkinter.CTkFrame(
             self, corner_radius=0, fg_color="transparent")
@@ -384,6 +372,4 @@ class App(customtkinter.CTk):
 if __name__ == "__main__":
     app = App()
     app.mainloop()
-
-
 
